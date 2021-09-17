6de1f14d06d865a06773ea734514176c96e2e006
brownandblue49 <64905307+brownandblue49@users.noreply.github.com>
2021-09-17 17:58:18 +0530

    Add files via upload

--- wsinterfaceproject/settings.py
+++ wsinterfaceproject/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
  
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'files'),
+    os.path.join(BASE_DIR, 'Files'),
 )
  
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
